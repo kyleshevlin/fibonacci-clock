b785f8cd8adf17317b84694c843e8c2dc1031093
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2017-02-26 22:40:08 -0800

    update instructions to dynamically show boxes withe right colors

--- app/js/components/Instructions.jsx
+++ app/js/components/Instructions.jsx
@@ -1,29 +1,68 @@
-import React from 'react'
+import React, { Component, PropTypes } from 'react'
+import { connect } from 'react-redux'
+import colors from '../constants/colors'
 
-const Instructions = () => (
-  <section className='instructions'>
-    <h3>How to Use a Fibonacci Clock</h3>
+class Instructions extends Component {
+  render () {
+    const boxStyles = {
+      display: 'inline-block',
+      width: 20,
+      height: 20,
+      marginBottom: '-2px'
+    }
+    const hoursBox = (
+      <span style={{
+        ...boxStyles,
+        backgroundColor: colors[this.props.colorKey][1]
+      }} />
+    )
+    const minutesBox = (
+      <span style={{
+        ...boxStyles,
+        backgroundColor: colors[this.props.colorKey][2]
+      }} />
+    )
+    const bothBox = (
+      <span style={{
+        ...boxStyles,
+        backgroundColor: colors[this.props.colorKey][3]
+      }} />
+    )
 
-    <p>
-      Believe it or not, you can tell time, in 5-minute increments, with just five boxes following the <a href='https://en.wikipedia.org/wiki/Fibonacci_number'>Fibonacci numbers</a>. Here's how:
-    </p>
+    return (
+      <section className='instructions'>
+        <h3>How to Use a Fibonacci Clock</h3>
 
-    <p>
-      Each square is worth the length of one of its sides. Thus, if we assign the smallest squares the value of 1, you end up with five boxes whose values are 1, 1, 2, 3, and 5.
-    </p>
+        <p>
+          Believe it or not, you can tell time, in 5-minute increments, with just five boxes following the <a href='https://en.wikipedia.org/wiki/Fibonacci_number'>Fibonacci numbers</a>. Here's how:
+        </p>
 
-    <p>
-      To calculate the hours, add up the value of the red and blue squares. For example, if you have a red 1-square and a blue 5-square, you have 6 hours.
-    </p>
+        <p>
+          Each square is worth the length of one of its sides. Thus, if we assign the smallest squares the value of 1, you end up with five boxes whose values are 1, 1, 2, 3, and 5.
+        </p>
 
-    <p>
-      Then, to calculate the minutes, add up the values of the green and blue squares and multiply this value by 5. For example, if you have a green 1-square, a blue 2-square, and a green 3-square, you have 30 minutes.
-    </p>
+        <p>
+          {`To calculate the hours, add up the value of the `}{hoursBox}{` and `}{bothBox}{` squares. For example, if you have a `}{hoursBox}{` worth 1, and a `}{bothBox}{` worth 5, you have 6 hours.`}
+        </p>
 
-    <p>
-      Together, these numbers represent the closest 5-minute increment using a standard 12-hour clock. Isn't that cool?
-    </p>
-  </section>
-)
+        <p>
+          {`Then, to calculate the minutes, add up the values of the `}{minutesBox}{` and `}{bothBox}{` squares and multiply this value by 5. For example, if you have a couple `}{minutesBox}{` squares worth 1 and 3, and a `}{bothBox}{` worth 2, you have 30 minutes.`}
+        </p>
 
-export default Instructions
+        <p>
+          Together, these numbers represent the closest 5-minute increment using a standard 12-hour clock. Isn't that cool?
+        </p>
+      </section>
+    )
+  }
+}
+
+Instructions.propTypes = {
+  colorKey: PropTypes.string
+}
+
+const mapStateToProps = state => ({
+  colorKey: state.colorKey
+})
+
+export default connect(mapStateToProps)(Instructions)
--- app/js/constants/colors.js
+++ app/js/constants/colors.js
@@ -1,4 +1,8 @@
 export default {
   default: ['#fff', '#f00', '#0f0', '#00f'],
-  monokai: ['#fff', '#f92672', '#a6e22e', '#66d9ef']
+  monokai: ['#fff', '#f92672', '#a6e22e', '#66d9ef'],
+  miami: ['#fff', '#b81769', '#abc93d', '#00aaad'],
+  entropy: ['#fff', '#930e07', '#6aaa42', '#152463'],
+  summit: ['#fff', '#ce1126', '#7ca982', '#3993dd'],
+  sandy: ['#f5f7dc', '#fc7753', '#a5cc68', '#4ecdc4']
 }
